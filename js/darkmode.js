15ab5c00ffee3cbece7fa239bdb328668d52bc8a
cuongquang2006-boop <cuongquang2006@gmail.com>
2026-06-16 18:13:47 +0700

    feat: remove folder

---
+++ js/darkmode.js
@@ -0,0 +1,6 @@
+const themeBtn =
+document.querySelector(".theme-btn");
+
+themeBtn.addEventListener(
+"click", ()=>{ document.body.classList.toggle("light-theme");
+})
--- portfolio_my/js/darkmode.js
+++
@@ -1,6 +0,0 @@
-const themeBtn =
-document.querySelector(".theme-btn");
-
-themeBtn.addEventListener(
-"click", ()=>{ document.body.classList.toggle("light-theme");
-})
